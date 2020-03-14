aea8d26352e7a70855f66f3bf99ffd5d86d206e7
ferdinandhubbard981 <ferdinandhubbard@gmail.com>
2020-03-14 10:07:04 +0000

    Add files via upload

---
+++ ATM/ATM/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ATM
+{
+
+    class UserAccount
+    {
+        protected float balance;
+        protected string firstname, surname;
+        
+        
+        public UserAccount(string inputFirstname, string inputSurname)
+        {
+            balance = 0f;
+            firstname = inputFirstname;
+            surname = inputSurname;
+        }
+        public float GetBalance()
+        {
+            return balance;
+        }
+        public void SetBalance(float newBalance)
+        {
+            balance = newBalance;
+        }
+
+        public bool WithDraw(float withdrawAmount)
+        {
+            if (GetBalance() >= withdrawAmount)
+            {
+
+            }
+        }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
